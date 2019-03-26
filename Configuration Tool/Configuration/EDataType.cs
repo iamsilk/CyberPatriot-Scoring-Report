b5d43a472bf469ff4da5b2a11466a67842741659
Stephen3495 <48915844+Stephen3495@users.noreply.github.com>
2019-03-26 10:10:06 -0300

    Added EDataType.cs for configuration parsing

---
+++ Configuration Tool/Configuration/EDataType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configuration_Tool.Configuration
+{
+    public enum EDataType
+    {
+        Boolean,
+        Byte,
+        ByteArray,
+        Char,
+        CharArray,
+        Decimal,
+        Double,
+        Int16,
+        Int32,
+        Int64,
+        SByte,
+        Single,
+        String,
+        UInt16,
+        UInt32,
+        UInt64
+    }
+}
