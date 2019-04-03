e61b2517175bce9100b48c428231c5c9b733a3de
Stephen3495 <48915844+Stephen3495@users.noreply.github.com>
2019-04-03 10:53:47 -0300

    Added GroupSettings class

---
+++ Configuration Tool/Configuration/Groups/GroupSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configuration_Tool.Configuration.Groups
+{
+    public class GroupSettings
+    {
+        public string GroupName { get; set; } = "";
+
+        public ScoredItem<List<IMember>> Members = new ScoredItem<List<IMember>>();
+    }
+}
