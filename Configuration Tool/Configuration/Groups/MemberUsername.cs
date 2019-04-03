12dc517b85c1d56a4bce4a1e4657e4fa8ecb5d78
Stephen3495 <48915844+Stephen3495@users.noreply.github.com>
2019-04-03 10:53:34 -0300

    Added MemberUsername class

---
+++ Configuration Tool/Configuration/Groups/MemberUsername.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configuration_Tool.Configuration.Groups
+{
+    public class MemberUsername : IIdentification, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnChange(string variable)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(variable));
+        }
+
+        public string IDType => "Username";
+
+        private string identifier = "";
+        public string Identifier
+        {
+            get { return identifier; }
+            set
+            {
+                if (identifier != value)
+                {
+                    identifier = value;
+                    OnChange("Identifier");
+                }
+            }   
+        }
+    }
+}
