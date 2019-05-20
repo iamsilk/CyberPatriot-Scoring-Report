a70a4b18582081d627f908c6f221fa664fc20e14
Stephen3495 <48915844+Stephen3495@users.noreply.github.com>
2019-05-20 16:20:09 -0300

    Added Setup function and ScoringSections list

--- Scoring Report/Scoring/ScoringManager.cs
+++ Scoring Report/Scoring/ScoringManager.cs
@@ -8,6 +8,32 @@ namespace Scoring_Report.Scoring
 {
     public static class ScoringManager
     {
+        public static List<ISection> ScoringSections { get; } = new List<ISection>();
+
         
+        public static void Setup()
+        {
+            ScoringSections.Clear();
+
+            // Get all types in the executing assembly (Scoring Report)
+            Type[] allTypes = Assembly.GetExecutingAssembly().GetTypes();
+            
+            foreach (Type type in allTypes)
+            {
+                // Filter to parent namespace "Scoring_Report.Scoring.Sections"
+                if (type.Namespace.StartsWith("Scoring_Report.Scoring.Sections"))
+                {
+                    // Filter to types with child interface ISection
+                    if (type.GetInterfaces().Contains(typeof(ISection)))
+                    {
+                        // Create instance
+                        ISection instance = Activator.CreateInstance(type) as ISection;
+
+                        // Add to list
+                        ScoringSections.Add(instance);
+                    }
+                }
+            }
+        }
     }
 }
