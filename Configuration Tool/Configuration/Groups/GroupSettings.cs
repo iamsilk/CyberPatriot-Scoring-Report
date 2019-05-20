aea50053068257586e07d1c9ac1fdce5746effef
Stephen3495 <48915844+Stephen3495@users.noreply.github.com>
2019-05-20 12:45:35 -0300

    Implemented INotifyPropertyChanged interface into GroupSettings class

--- Configuration Tool/Configuration/Groups/GroupSettings.cs
+++ Configuration Tool/Configuration/Groups/GroupSettings.cs
@@ -6,10 +6,48 @@ using System.Threading.Tasks;
 
 namespace Configuration_Tool.Configuration.Groups
 {
-    public class GroupSettings
+    public class GroupSettings : INotifyPropertyChanged
     {
-        public string GroupName { get; set; } = "";
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected void OnChange(string variable)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(variable));
+        }
 
-        public ScoredItem<List<IMember>> Members = new ScoredItem<List<IMember>>();
+        private string groupName = "";
+        public string GroupName
+        {
+            get { return groupName; }
+            set
+            {
+                if (groupName != value)
+                {
+                    groupName = value;
+                    OnChange("GroupName");
+                }
+            }
+        }
+
+        private bool isScored = false;
+        public bool IsScored
+        {
+            get { return isScored; }
+            set
+            {
+                if (isScored != value)
+                {
+                    isScored = value;
+                    OnChange("IsScored");
+                }
+            }
+        }
+
+        public List<IMember> Members = new List<IMember>();
+
+        public GroupSettings()
+        {
+
+        }
     }
 }
