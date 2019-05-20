f4748d8d85c1b4ffb3ed0ae278830ef68418def3
Stephen3495 <48915844+Stephen3495@users.noreply.github.com>
2019-05-20 15:08:49 -0300

    Added SectionDetails.cs
    Used as a return value for every section class's GetScore function

---
+++ Scoring Report/Scoring/SectionDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scoring_Report.Scoring
+{
+    public class SectionDetails
+    {
+        public int Points = 0;
+
+        public string Output = "";
+
+        public SectionDetails(int points, string output)
+        {
+            Points = points;
+            Output = output;
+        }
+    }
+}
