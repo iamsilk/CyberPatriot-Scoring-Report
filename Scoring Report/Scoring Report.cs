09594b40cd9603c930dc39a79a0d164c084b803a
Stephen3495 <48915844+Stephen3495@users.noreply.github.com>
2019-05-20 13:10:46 -0300

    Added Scoring Report project

---
+++ Scoring Report/Scoring Report.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Scoring_Report
+{
+    public partial class ScoringReport : ServiceBase
+    {
+        public ScoringReport()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+    }
+}
